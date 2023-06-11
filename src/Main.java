51c68e3a27ccb76f2bda909355df1ce839528aa9
valeri05 <132645004+valeri05@users.noreply.github.com>
2023-06-11 11:55:24 +0300

    Removed code duplication Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,21 @@ public class Main {
         int balance = 100; // текущий счет клиента
         int bonus = 0; // каждые 100 рублей пополнения = 1 бонусный рубль
         int replenishment = 1100; // пополнение в рублях
+        int totalBalance = (balance + replenishment); // итого на балансе денежных средств
+        int bonusBalance;
 
         if (replenishment >= 1000) {
-            int bonusBalance = (bonus + (replenishment / 100));
-            int totalBalance = (balance + replenishment);
-
-            System.out.println("Баланс:");
-            System.out.println(balance + " руб.");
-            System.out.println("Пополнение:");
-            System.out.println(replenishment + " руб.");
-            System.out.println("Баланс бонусов:");
-            System.out.println(bonusBalance + " бонусных руб.");
-            System.out.println("Итого на балансе:");
-            System.out.println(totalBalance + " руб.");
+            bonusBalance = (bonus + (replenishment / 100));
         } else {
-
-            int bonusBalance = (bonus);
-            int totalBalance = (balance + replenishment);
-
-            System.out.println("Баланс:");
-            System.out.println(balance + " руб.");
-            System.out.println("Пополнение:");
-            System.out.println(replenishment + " руб.");
-            System.out.println("Баланс бонусов:");
-            System.out.println(bonusBalance + " бонусных руб.");
-            System.out.println("Итого на балансе:");
-            System.out.println(totalBalance + " руб.");
-
+            bonusBalance = (bonus);
         }
+        System.out.println("Баланс:");
+        System.out.println(balance + " руб.");
+        System.out.println("Пополнение:");
+        System.out.println(replenishment + " руб.");
+        System.out.println("Баланс бонусов:");
+        System.out.println(bonusBalance + " бонусных руб.");
+        System.out.println("Итого на балансе:");
+        System.out.println(totalBalance + " руб.");
     }
 }
